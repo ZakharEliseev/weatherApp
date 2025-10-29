f7411edfd7e8e791ea40f0e2b5b232922ffa7270
ZakharEliseev <eliseev.zv@gmail.com>
2025-10-29 10:23:27 +0500

    refactor

--- src/scripts/UiService.ts
+++ src/scripts/UiService.ts
@@ -29,9 +29,7 @@ export class UiService {
     children: (HTMLElement | string)[] = [],
   ): HTMLElement {
     const element = document.createElement(tag);
-    if (className) {
-      element.classList.add(className);
-    }
+    element.classList.add(className ?? null);
     element.textContent = content ?? null;
     children.forEach((elem) => element.append(elem));
     return element;
@@ -62,12 +60,6 @@ export class UiService {
     this.renderCalendar(forecast);
     this.renderCityName(cityName);
     this.renderWeather(forecast[this.activeDay]);
-  }
-
-  createLi(forecast: GroupedForecast) {
-    this.calendarBlock.replaceChildren();
-    const days = Object.keys(forecast);
-    
   }
 
   renderCalendar(forecast: GroupedForecast): void {
@@ -108,75 +100,37 @@ export class UiService {
   renderWeather(data: WeatherEntry[]): void {
     this.weatherBlock.replaceChildren();
     data.forEach((d: any) => {
-      const li = document.createElement('li');
-      li.classList.add('weather-item');
-
-      const temp = document.createElement('div');
-      temp.classList.add('weather-temp');
-
-      const time = document.createElement('p');
-      time.classList.add('weather-temp_time');
-      time.textContent = d.time;
-
-      const degree = document.createElement('p');
-      degree.classList.add('weather-temp_degree');
-      degree.textContent = d.temp + ' °C';
-
-      temp.append(time, degree);
-
-      const description = document.createElement('div');
-      description.classList.add('weather-descr');
-
-      const descriptionIcon = document.createElement('img');
-      descriptionIcon.classList.add('weather-descr_icon');
-      descriptionIcon.src = `./src/img/icons/${d.icon}@2x.png`;
-
-      const descriptionText = document.createElement('p');
-      descriptionText.classList.add('weather-descr-text');
-      description.textContent = d.description;
-
-      description.append(descriptionIcon, descriptionText);
-
-      const wind = document.createElement('div');
-      wind.classList.add('weather-wind');
-
-      const windTitle = document.createElement('p');
-      windTitle.classList.add('weather-wind_title');
-      wind.textContent = 'Скорость ветра';
-
-      const windMetric = document.createElement('p');
-      windMetric.classList.add('weather-wind_metric');
-      windMetric.textContent = d.wind + ' м/с';
-
-      wind.append(windTitle, windMetric);
-
-      const pressure = document.createElement('div');
-      pressure.classList.add('weather-pressure');
-
-      const pressureTitle = document.createElement('p');
-      pressureTitle.classList.add('weather-pressure_title');
-      pressureTitle.textContent = 'Атмосферное давление';
-
-      const pressureMetric = document.createElement('p');
-      pressureMetric.classList.add('weather-pressure_metric');
-      pressureMetric.textContent = d.pressure + ' мм рт.';
-
-      pressure.append(pressureTitle, pressureMetric);
-
-      const humidity = document.createElement('div');
-      humidity.classList.add('weather-humidity');
-
-      const humidityTitle = document.createElement('p');
-      humidityTitle.classList.add('weather-humidity_title');
-      humidityTitle.textContent = 'Влажность';
-
-      const humidityMetric = document.createElement('p');
-      humidityMetric.classList.add('weather-humidity_metric');
-      humidityMetric.textContent = d.humidity + ' %';
-
-      humidity.append(humidityTitle, humidityMetric);
-
-      li.append(temp, description, wind, pressure, humidity);
+      
+      const temp = this.div('weather-temp', [
+        this.p('weather-temp_time', d.time),
+        this.p('weather-temp_degree', d.temp + ' °C'),
+      ]);
+      
+      const icon = document.createElement('img') as HTMLImageElement;
+      icon.classList.add('weather-descr_icon');
+      icon.src = `./src/img/icons/${d.icon}@2x.png`;
+      
+      const description = this.div('weather-descr', [
+        this.p('weather-descr-text', d.description),
+        icon,
+      ]);
+      
+      const wind = this.div('weather-wind', [
+        this.p('weather-wind_title', 'Скорость ветра'),
+        this.p('weather-wind_metric', d.wind + ' м/с'),
+      ]);
+      
+      const pressure = this.div('weather-pressure', [
+        this.p('weather-pressure_title', 'Атмосферное давление'),
+        this.p('weather-pressure_metric', d.pressure + ' мм рт.'),
+      ]);
+      
+      const humidity = this.div('weather-humidity', [
+        this.p('weather-humidity_title', 'Влажность'),
+        this.p('weather-humidity_metric', d.humidity + ' %')
+      ]);
+      
+      const li = this.elementCreator('li', { className: 'weather-item' }, [temp, description, wind, pressure, humidity]);
       this.weatherBlock.append(li);
     });
   }
